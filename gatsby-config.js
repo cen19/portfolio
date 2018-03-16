f441d1647c4b3e5ed3ecf4936034fdd1af1d94b0
cen19 <wilsoncen@gmail.com>
2018-03-16 16:24:05 -0700

    Initial scaffolding

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Wilson Cen\'s Portfolio',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography.js',
+      },
+    },
+  ],
+};
