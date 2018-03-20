11a852e7913b44becaf45bb4f0ff96d971455d7e
cen19 <wilsoncen@gmail.com>
2018-03-20 16:39:15 -0700

    Added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,16 @@ module.exports = {
       resolve: 'gatsby-plugin-typography',
       options: {
         pathToConfigModule: 'src/utils/typography.js',
-      },
+      }
     },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'keygoeshere',
+        // head: false, // optional
+        // anonymize: true, // optional
+        // respectDNT: true,
+      }
+    }
   ],
 };
